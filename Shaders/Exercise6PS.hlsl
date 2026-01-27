1c8f7e9d7b43dae3324822965cea07bf6dbeb5cd
David Varo <david.vama21@gmail.com>
2026-01-27 11:58:16 +0100

    Changed pixel shader to use my version of Phong shading, with issues fixed.

--- Shaders/Exercise6PS.hlsl
+++ Shaders/Exercise6PS.hlsl
@@ -5,7 +5,7 @@ SamplerState colourSamp : register(s0);
 
 float4 main(float3 worldPos : POSITION, float3 normal : NORMAL, float2 coord : TEXCOORD) : SV_TARGET
 {
-    /*
+    
     normal = normalize(normal); // because it was interpolated and might not be normalized
     float3 surfaceColor = hasDiffuseTex ? colourTex.Sample(colourSamp, coord).rgb * diffuseColour.rgb : diffuseColour.rgb;
     float minNormPerLight = -dot(normal, L);
@@ -16,14 +16,14 @@ float4 main(float3 worldPos : POSITION, float3 normal : NORMAL, float2 coord : T
         float3 observerDir = normalize(viewPos - worldPos); // V
         float3 lightReflectionDir = reflect(L, normal); // R
         
-        color = Ac * surfaceColor + Kd * surfaceColor * Lc * minNormPerLight + Ks * Lc * pow (dot(observerDir,lightReflectionDir), shininess);
+        color = Ac * surfaceColor + Kd * surfaceColor * Lc * minNormPerLight + Ks * Lc * pow (saturate(dot(observerDir,lightReflectionDir)), shininess);
         
     }else
         color = Ac * surfaceColor;
     
-    return float4(color, 1.0)
-    */
+    return float4(color, 1.0);
     
+    /*
     float3 Cd = hasDiffuseTex ? colourTex.Sample(colourSamp, coord).rgb * diffuseColour.rgb : diffuseColour.rgb;
     float3 N = normalize(normal); // because it was interpolated and might not be normalized
     float3 R = reflect(L, N);
@@ -34,4 +34,5 @@ float4 main(float3 worldPos : POSITION, float3 normal : NORMAL, float2 coord : T
     float3 colour = Cd * Kd * dotNL * Lc + Ac * Cd + Ks * Lc * pow(dotVR, shininess);
 
     return float4(colour, 1.0);
+    */
 }
